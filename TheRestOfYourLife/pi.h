65ac4e029356e068214b2c062a3e6775769b6be2
André <42980905+AndreKoza@users.noreply.github.com>
2020-04-29 10:03:01 +0200

    Stratified MC

--- TheRestOfYourLife/pi.h
+++ TheRestOfYourLife/pi.h
@@ -9,19 +9,25 @@
 void pi_main()
 {
 	int inside_circle = 0;
-	int runs = 0;
-	std::cout << std::fixed << std::setprecision(12);
-	while (true)
+	int inside_circle_stratified = 0;
+	int sqrt_N = 10000;
+	for (int i = 0; i < sqrt_N; ++i)
 	{
-		++runs;
-		auto x = random_double(-1, 1);
-		auto y = random_double(-1, 1);
-		if (x * x + y * y < 1)
-			++inside_circle;
+		for (int j = 0; j < sqrt_N; ++j)
+		{
+			auto x = random_double(-1, 1);
+			auto y = random_double(-1, 1);
+			if (x * x + y * y < 1)
+				++inside_circle;
 
-		if (runs % 100000 == 0)
-			std::cout << "Estimate of Pi = " << 4 * double(inside_circle) / runs << '\n';
+			x = 2 * ((i + random_double()) / sqrt_N) - 1;
+			y = 2 * ((j + random_double()) / sqrt_N) - 1;
+			if (x * x + y * y < 1)
+				++inside_circle_stratified;
+		}
 	}
-
-	
+	auto N = std::pow(sqrt_N, 2);
+	std::cout << std::fixed << std::setprecision(12);
+	std::cout	<< "Regular    Estimate of Pi = " << 4 * double(inside_circle) / N << '\n'
+				<< "Stratified Estimate of Pi = " << 4 * double(inside_circle_stratified) / N << '\n';
 }
